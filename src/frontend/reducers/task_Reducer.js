ff0971bf55ffe25fb258541ad7b403235b101a82
prathmesh <prathmeshjagtap405@gmail.com>
2022-04-21 12:18:38 +0530

    fix-updated switch cases

--- src/frontend/reducers/task_Reducer.js
+++ src/frontend/reducers/task_Reducer.js
@@ -3,20 +3,8 @@ import { taskConstants } from "../constants";
 const task_Reducer = (state, action) => {
 	switch (action.type) {
 		case taskConstants.GET_TASK:
-			return {
-				...state,
-				tasks: action.payload,
-			};
 		case taskConstants.ADD_TASK:
-			return {
-				...state,
-				tasks: action.payload,
-			};
 		case taskConstants.EDIT_TASK:
-			return {
-				...state,
-				tasks: action.payload,
-			};
 		case taskConstants.DELETE_TASK:
 			return {
 				...state,
