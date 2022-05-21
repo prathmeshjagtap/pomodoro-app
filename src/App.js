eef2678e217b2aa20002e2df1928eb9884b95c53
prathmesh <prathmeshjagtap405@gmail.com>
2022-05-21 15:21:25 +0530

    added title sync with pomodor timer

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import "./App.css";
 import { AllRoutes } from "./frontend/Routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useLocation } from "react-router-dom";
+import { useAuthContext } from "./frontend/context";
+import { useEffect } from "react";
 
 function App() {
+	const { pathname } = useLocation();
+	const {
+		authState: { token },
+	} = useAuthContext();
+	useEffect(() => {
+		if (!token || pathname !== "/pomodoro") {
+			document.title = "✅ Tomato";
+		}
+	}, [token, pathname]);
+
 	return (
 		<div className="bg-blue-400 dark:bg-neutral-800 dark:text-white min-h-screen ">
 			<AllRoutes />
